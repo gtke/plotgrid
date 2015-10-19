2b10185d10683347270b412a760af7302a2499e1
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2015-10-19 00:31:24 -0400

    Add default port for heroku

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, jsonify, render_template
+import os
 import requests
 import json
+
+from flask import Flask, jsonify, render_template
 from datetime import datetime
 from flaskrun import create_app
+
 
 app = create_app()
 app.config.from_object('config')
 
+
 TIME_WINDOW = app.config['time_window']
 ENDPOINT = app.config['ENDPOINT']
+
 
 @app.route('/', methods=['GET'])
 def get():
@@ -27,5 +32,7 @@ def pong():
         'status': 'alive'
     })
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
